7e6adc15594b58c2cb06e31385b0d11675ba607c
Joâo Marcos Silva de Oliveira <joaomarcoscsilva10@gmail.com>
2024-04-10 20:53:54 -0300

    teste 1

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function playRockPaperScissor(player1,player2){
-    if (player1.toUpperCase() == "PEDRA" && player2.toUpperCase() == "TESOURA")
-    console.log ()
+    if (player1.toUpperCase() == "PEDRA" && player2.toUpperCase() == "TESOURA");
+    console.log ('Jogador 1 venceu');
+    return
+
 }
 
