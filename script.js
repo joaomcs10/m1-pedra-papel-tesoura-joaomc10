271c1dbc80fea5929fa4bf86a1d2b95923d01430
Joâo Marcos Silva de Oliveira <joaomarcoscsilva10@gmail.com>
2024-04-10 21:49:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 function playRockPaperScissor(player1,player2){
-    if (player1.toUpperCase() == "PEDRA" && player2.toUpperCase() == "TESOURA");
-    console.log ('Jogador 1 venceu');
-    return
+    
+    if ((player1.toUpperCase() == "PAPEL" && player2.toUpperCase() == "PAPEL")){
+        console.log ('EMPATOU!');
+        return;
+
+    }if else((player1.toUpperCase() == "TESOURA" && player2.toUpperCase() == "TESOURA")){
+        console.log ('EMPATOU!');
+        return;
+
+    }
+        
+     
+    if else(player1.toUpperCase() == "PEDRA" && player2.toUpperCase() == "PEDRA");
+     console.log ('EMPATOU!');
+     return;
+    if else(player1.toUpperCase() == "PEDRA" && player2.toUpperCase() == "PAPEL");
+     console.log ('Jogador 2 venceu');
+     return;
+    if else(player1.toUpperCase() == "PAPEL" && player2.toUpperCase() == "PEDRA");
+     console.log ('Jogador 1 venceu');
+     return;
+    if else(player1.toUpperCase() == "PEDRA" && player2.toUpperCase() == "TESOURA");
+     console.log ('Jogador 1 venceu');
+     return;
+    if else(player1.toUpperCase() == "TESOURA" && player2.toUpperCase() == "PEDRA");
+     console.log ('Jogador 2 venceu');
+     return;
+    if else(player1.toUpperCase() == "PAPEL" && player2.toUpperCase() == "TESOURA");
+     console.log ('Jogador 2 venceu');
+     return;
+    if else(player1.toUpperCase() == "TESOURA" && player2.toUpperCase() == "PAPEL");
+     console.log ('Jogador 1 venceu');
+     return;
 
 }
-playRockPaperScissor(pedra,tesoura);
-console.log(playRockPaperScissor(pedra,tesoura));
